968109fb79a054b4ff22f258b80dbeaaf0f37f1d
alexismalzieu <alex.malzieu@gmail.com>
2021-01-10 11:42:56 +0100

    🐛 fix ts param error

--- src/seeds/script.ts
+++ src/seeds/script.ts
@@ -3,7 +3,7 @@ import { postRecipe } from '../services/RecipeService'
 import * as data from './data.json'
 
 data.recipes.forEach((recipe) => {
-  postRecipe(recipe, recipe.image).catch((error) => console.error(error))
+  postRecipe(recipe).catch((error) => console.error(error))
 })
 
 export default data
