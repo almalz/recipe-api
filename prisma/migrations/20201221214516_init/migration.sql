fce3e929c9bb1d57328dda27d09b8a815b7f2359
almalz <alex.malzieu@gmail.com>
2021-03-27 23:38:46 +0100

    🗃 update prisma

--- prisma/migrations/20201221214516_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Recipe" ALTER COLUMN "instructionId" DROP NOT NULL;
--- prisma/migrations/20210327220645_init/migration.sql
+++ prisma/migrations/20210327220645_init/migration.sql
@@ -2,21 +2,31 @@
 CREATE TYPE "System" AS ENUM ('METRIC', 'US');
 
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firebaseId" TEXT NOT NULL,
+    "email" TEXT,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Recipe" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "prepTime" INTEGER,
     "cookingTime" INTEGER,
-    "instructionId" INTEGER NOT NULL,
+    "instructionId" INTEGER,
     "imageUrl" TEXT,
     "sourceUrl" TEXT,
+    "userId" INTEGER,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Ingredient" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "alternativeingredientId" INTEGER,
 
@@ -25,7 +35,7 @@ CREATE TABLE "Ingredient" (
 
 -- CreateTable
 CREATE TABLE "IngredientsOnRecipe" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "recipeId" INTEGER NOT NULL,
     "ingredientId" INTEGER,
     "base_amout" INTEGER,
@@ -36,7 +46,7 @@ CREATE TABLE "IngredientsOnRecipe" (
 
 -- CreateTable
 CREATE TABLE "Unit" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "system" "System" NOT NULL,
     "symbol" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -46,7 +56,7 @@ CREATE TABLE "Unit" (
 
 -- CreateTable
 CREATE TABLE "Tag" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
 
     PRIMARY KEY ("id")
@@ -54,7 +64,7 @@ CREATE TABLE "Tag" (
 
 -- CreateTable
 CREATE TABLE "Instruction" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "recipeId" INTEGER NOT NULL,
 
     PRIMARY KEY ("id")
@@ -62,7 +72,7 @@ CREATE TABLE "Instruction" (
 
 -- CreateTable
 CREATE TABLE "InstructionStep" (
-"id" SERIAL,
+    "id" SERIAL NOT NULL,
     "body" TEXT NOT NULL,
     "instructionId" INTEGER NOT NULL,
 
@@ -74,6 +84,12 @@ CREATE TABLE "_RecipeToTag" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.firebaseId_unique" ON "User"("firebaseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Ingredient.name_unique" ON "Ingredient"("name");
@@ -94,25 +110,28 @@ CREATE UNIQUE INDEX "_RecipeToTag_AB_unique" ON "_RecipeToTag"("A", "B");
 CREATE INDEX "_RecipeToTag_B_index" ON "_RecipeToTag"("B");
 
 -- AddForeignKey
-ALTER TABLE "Recipe" ADD FOREIGN KEY("instructionId")REFERENCES "Instruction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Recipe" ADD FOREIGN KEY ("instructionId") REFERENCES "Instruction"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Ingredient" ADD FOREIGN KEY("alternativeingredientId")REFERENCES "Ingredient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Recipe" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "IngredientsOnRecipe" ADD FOREIGN KEY("recipeId")REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Ingredient" ADD FOREIGN KEY ("alternativeingredientId") REFERENCES "Ingredient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "IngredientsOnRecipe" ADD FOREIGN KEY("ingredientId")REFERENCES "Ingredient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "IngredientsOnRecipe" ADD FOREIGN KEY ("ingredientId") REFERENCES "Ingredient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "IngredientsOnRecipe" ADD FOREIGN KEY("unitId")REFERENCES "Unit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "IngredientsOnRecipe" ADD FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "InstructionStep" ADD FOREIGN KEY("instructionId")REFERENCES "Instruction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "IngredientsOnRecipe" ADD FOREIGN KEY ("unitId") REFERENCES "Unit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_RecipeToTag" ADD FOREIGN KEY("A")REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "InstructionStep" ADD FOREIGN KEY ("instructionId") REFERENCES "Instruction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_RecipeToTag" ADD FOREIGN KEY("B")REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_RecipeToTag" ADD FOREIGN KEY ("A") REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RecipeToTag" ADD FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
